6d977af36103cde228026609339225dbf0d2c0fc
Gregory Krymov <62312237+pingiy@users.noreply.github.com>
2021-03-10 23:15:14 +0300

    Update myip.sh

--- myip.sh
+++ myip.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 # myip.sh,
-# v1.2 #agh0r3@proton mail.com,
-# скрипт возвращает ISO 3166-2 код страны, ip адреса машины, на которой запущен,
+# v1.2 # by pigiy
+# the script do request to wtfismyip.com service, returns ISO 3166-2 country code, ip address of the machine on which it is running,
 # (https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2#Officially_assigned_code_elements)
 
-# пишет вывод (2 символа) который можно выводить в панель xfce, для мониторинга вашего айпи, в элемент 'общий монитор',
-# добавив команду 'sh [/путь-к-скрипту/./myip.sh]',
-# и обязательно выставить интервал обновления элемента 'общий монитор', не менее 60 секунд,
-# иначе wtfismyip.com может забанить запросы от вашего IP, согласно регламенту автоматизации сервиса:
+# script returns output (2 characters) that can be added to the xfce panel, for monitoring your ip address country when you connected over vpn.
+# adding the command 'sh [/path-k-script/./myip.sh]', to the 'shared monitor' element in xfce panel.
+# and be sure to set the refresh interval for the 'shared monitor' element, at least 60 seconds,
+# otherwise wtfismyip.com can ban requests from your IP, according to the service automation regulations:
 
 # "All we ask is that you limit usage to 1 request per minute, per IP address.
-# Any usage in excess of this may lead to a temporary block."
-# (https://wtfismyip.com/automation )
+# Any usage in excess of this may lead to a temporary block. "
+# (https://wtfismyip.com/automation)
 
-# требует: ping, curl, whois, awk,
-# использование: сделать скртипт исполняемым: 'chmod +x myip
-# и запустить: ./myip.sh'
+# requires: ping, curl, whois, awk,
+# usage: make script executable: 'chmod + x myip.sh
+# and run: ./myip.sh '
 
 MYIP=https://wtfismyip.com/text
-PING_URL=t.me
+PING_URL=wtfismyip.com
 
 PING=
 PING=$(ping $PING_URL -q -c1 -w3 -W3)
