5c46b9a0daccdfe23e088088cb1d3dd931adb3f9
Gregory Krymov <62312237+pingiy@users.noreply.github.com>
2021-06-01 02:31:23 +0300

    Update myip_countrycode.sh

--- myip_countrycode.sh
+++ myip_countrycode.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # myip_countrycode.sh,
 # v1.1 # by pingiy
 # This script makes request to wtfismyip.com service, returns ISO 3166-2 country code, ip address of the machine on which it is running,
